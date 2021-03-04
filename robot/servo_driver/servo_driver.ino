17e58a729d387ff0fa07b13e65c2873513306cbf
Massimiliano D'Amico <massimiliano.damico@exprivia.it>
2021-03-04 16:29:42 +0100

    servo

--- robot/servo_driver/servo_driver.ino
+++ robot/servo_driver/servo_driver.ino
@@ -1,21 +1,27 @@
-//IDE ESP32
-//https://randomnerdtutorials.com/installing-the-esp32-board-in-arduino-ide-windows-instructions/
+//Preparazione IDE ESP32:
 //File > Preferences > Additional Board Manager URLs > https://dl.espressif.com/dl/package_esp32_index.json
-//Board > Boards Manager > ESP32
-//Tools > Board: "DOIT ESP32 DEVKIT V1" > DOIT ESP32 DEVKIT V1
-//Tools > Port > Selezionare porta com
+//Tools > Board Arduino Uno > Boards Manager > ESP32
+//Tools > Board Arduino Uno > ESP32 Arduino" > DOIT ESP32 DEVKIT V1
+//Tools > Manage Libraries > PubSubClient
+//Tools > Manage Libraries > ArduinoJson
+//Tools > Manage Libraries > Adafruit PWM Servo Driver Library by Adafruit
+//Tools > Port > <Selezionare porta seriale disponibile>
 
-//https://github.com/adafruit/Adafruit-PWM-Servo-Driver-Library
+//links:
+//https://randomnerdtutorials.com/installing-the-esp32-board-in-arduino-ide-windows-instructions/
+//https://techtutorialsx.com/2017/04/24/esp32-connecting-to-a-wifi-network/
+//https://learn.adafruit.com/16-channel-pwm-servo-driver/using-the-adafruit-library
 //https://robojax.com/learn/arduino/?vid=robojax_PCA9685-V4
-//https://arduinojson.org/v6/doc/installation/
-//https://github.com/bblanchon/ArduinoJson
-//https://github.com/knolleary/pubsubclient
+//https://www.pubnub.com/blog/pubsub-nodemcu-32s-esp32-mqtt-pubnub-arduino-sdk/
+//https://arduinojson.org/v6/example/string/
 
-#include <ESP8266WiFi.h>
+#include <WiFi.h>
 #include <PubSubClient.h>
-#include <ArduinoJson.h>
+
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
+
+#include <ArduinoJson.h>
 
 //servo range
 #define DEGREEMIN -90 //'minimum' degree
@@ -35,57 +41,67 @@
 #define LEFT_BACK_SHOULDER 	 8
 #define LEFT_BACK_ARM	     9
 
-// called this way, it uses the default address 0x40
-Adafruit_PWMServoDriver board = Adafruit_PWMServoDriver(0x40);
-
-//configuration
+//wifi and mqtt configuration
 const char* ssid     = "Vodafone-C01960075";
 const char* password = "tgYsZkgHA4xhJLGy";
 const char* mqtt_server = "test.mosquitto.org";
+const int mqtt_port = 1883;
 const char* topic_input = "myhome/mx/cserver";
 const char* topic_output = "myhome/mx/cserver";
+const char* client_id = "bb7-2.0/servo-driver/123456";
 
-WiFiClient espClient;
-PubSubClient client(espClient);
+//mqtt vars
+WiFiClient MQTTclient;
+PubSubClient client(MQTTclient);
+long lastReconnectAttempt = 0;
 
-//move body joint
-void moveJoint(joint, degree) {
-    sign = 1
-    if(joint==NECK || joint==LEFT_FRONT_ARM || joint==RIGHT_BACK_ARM || joint==RIGHT_FRONT_SHOULDER || joint==RIGHT_BACK_SHOULDER]){
-        sign = -1;
-	}
-	int pulse = map(degree*sign, DEGREEMIN, DEGREEMAX, SERVOMIN, SERVOMAX);
-    board.setPWM(joint, 0, pulse);
-}
+//adafruit servo motor driver vars
+//called this way, it uses the default address 0x40
+Adafruit_PWMServoDriver board = Adafruit_PWMServoDriver(0x40);
 
 //setup wifi
 void setup_wifi() {
-
-  delay(10);
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  randomSeed(micros());
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-  
-  Serial.println("Servoboard init!");
-  board.begin();
-  board.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
+  Serial.begin(115200);
  
+  WiFi.begin(ssid, password);
+  
+ if(WiFi.waitForConnectResult() != WL_CONNECTED) {
+      Serial.println("Couldn't connect to WiFi.");
+      while(1) delay(100);
+  }
+ 
+  Serial.println("Connected to the WiFi network");
+}
+
+//setup mqtt
+void setup_mqtt() {
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+  lastReconnectAttempt = 0;
+}
+
+//setup adafruit board
+void setup_adafruit_board() {
+  board.begin();
+  board.setPWMFreq(60);
+}
+
+//reconnect to mqtt
+boolean reconnect() {
+  if (client.connect(client_id)) {
+    client.subscribe(topic_output); // Subscribe to channel.
+  }
+  return client.connected();
+}
+
+//move body joint (servo motor)
+void moveJoint(int joint, int degree) {
+  int sign = 1;
+  if(joint==NECK || joint==LEFT_FRONT_ARM || joint==RIGHT_BACK_ARM || joint==RIGHT_FRONT_SHOULDER || joint==RIGHT_BACK_SHOULDER){
+    sign = -1;
+  }
+  int pulse = map(degree*sign, DEGREEMIN, DEGREEMAX, SERVOMIN, SERVOMAX);
+  board.setPWM(joint, 0, pulse);
 }
 
 //mqtt message callback
@@ -93,147 +109,130 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
+  String payload_buff;
+  for (int i=0;i<length;i++) {
+    payload_buff = payload_buff+String((char)payload[i]);
   }
-  Serial.println();
   
   //test servos lite
-  char json[] = (char)payload[0];
-  if((char)payload[0] == 'testservo') {
-	for(int i=0; i<10; i++){  
-		moveJoint(i, 0);  
-		delay(5000);		
-		moveJoint(i, -10);  
-		delay(5000);	
-		moveJoint(i, 0);  
-		delay(5000);	
-		moveJoint(i, +10);  
-		delay(5000);	
-	}
-	return;
+  if(payload_buff == "testservo") {
+  	for(int i=0; i<10; i++){  
+  		moveJoint(i, 0);  
+  		delay(5000);		
+  		moveJoint(i, -10);  
+  		delay(5000);	
+  		moveJoint(i, 0);  
+  		delay(5000);	
+  		moveJoint(i, +10);  
+  		delay(5000);	
+  	}
+  	return;
   }
   
   //test servos full
-  char json[] = (char)payload[0];
-  if((char)payload[0] == 'testservofull') {
-	for(int i=0; i<10; i++){      
-		for(int angle=-90; angle<=90; angle+=10){
-			moveJoint(i, angle);
-			delay(5000);
-		}
+  if(payload_buff == "testservofull") {
+  	for(int i=0; i<10; i++){      
+  		for(int angle=-90; angle<=90; angle+=10){
+  			moveJoint(i, angle);
+  			delay(5000);
+  		}
     }
-	return;
+  	return;
   }
   
   //test led
-  if((char)payload[0] == 'testledOn') {
+  if(payload_buff == "testledOn") {
     digitalWrite(BUILTIN_LED, LOW);
-	return;
+	  return;
   }
-  if((char)payload[0] == 'testledOff') {
+  if(payload_buff == "testledOff") {
     digitalWrite(BUILTIN_LED, HIGH);
-	return;
+	  return;
   }
   
   //get json payload
-  char json[] = (char)payload[0];
-  StaticJsonDocument<200> doc;
-  DeserializationError error = deserializeJson(doc, json);
+  DynamicJsonDocument doc(1024);
+  DeserializationError error = deserializeJson(doc, payload_buff);
   JsonObject root = doc.to<JsonObject>();
   
   //translate json to servo moves
   if(root.containsKey("HEAD")) {
-	int degree = root["HEAD"].as<int>();
-	moveJoint(HEAD, degree);
+	  int degree = root["HEAD"].as<int>();
+	  moveJoint(HEAD, degree);
   }
   if(root.containsKey("NECK")) {
-	int degree = root["NECK"].as<int>();
-	moveJoint(NECK, degree);
+	  int degree = root["NECK"].as<int>();
+	  moveJoint(NECK, degree);
   }
   if(root.containsKey("RIGHT_FRONT_SHOULDER")) {
-	int degree = root["RIGHT_FRONT_SHOULDER"].as<int>();
-	moveJoint(RIGHT_FRONT_SHOULDER, degree);
+	  int degree = root["RIGHT_FRONT_SHOULDER"].as<int>();
+	  moveJoint(RIGHT_FRONT_SHOULDER, degree);
   }
   if(root.containsKey("RIGHT_FRONT_ARM")) {
-	int degree = root["RIGHT_FRONT_ARM"].as<int>();
-	moveJoint(RIGHT_FRONT_ARM, degree);
+	  int degree = root["RIGHT_FRONT_ARM"].as<int>();
+	  moveJoint(RIGHT_FRONT_ARM, degree);
   }
   if(root.containsKey("LEFT_FRONT_SHOULDER")) {
-	int degree = root["LEFT_FRONT_SHOULDER"].as<int>();
-	moveJoint(LEFT_FRONT_SHOULDER, degree);
+	  int degree = root["LEFT_FRONT_SHOULDER"].as<int>();
+	  moveJoint(LEFT_FRONT_SHOULDER, degree);
   }
   if(root.containsKey("LEFT_FRONT_ARM")) {
 	int degree = root["LEFT_FRONT_ARM"].as<int>();
 	moveJoint(LEFT_FRONT_ARM, degree);
   }
   if(root.containsKey("RIGHT_BACK_SHOULDER")) {
-	int degree = root["RIGHT_BACK_SHOULDER"].as<int>();
-	moveJoint(RIGHT_BACK_SHOULDER, degree);
+	  int degree = root["RIGHT_BACK_SHOULDER"].as<int>();
+	  moveJoint(RIGHT_BACK_SHOULDER, degree);
   }
   if(root.containsKey("RIGHT_BACK_ARM")) {
-	int degree = root["RIGHT_BACK_ARM"].as<int>();
-	moveJoint(RIGHT_BACK_ARM, degree);
+	  int degree = root["RIGHT_BACK_ARM"].as<int>();
+	  moveJoint(RIGHT_BACK_ARM, degree);
   }
   if(root.containsKey("LEFT_BACK_SHOULDER")) {
-	int degree = root["LEFT_BACK_SHOULDER"].as<int>();
-	moveJoint(LEFT_BACK_SHOULDER, degree);
+	  int degree = root["LEFT_BACK_SHOULDER"].as<int>();
+	  moveJoint(LEFT_BACK_SHOULDER, degree);
   }
   if(root.containsKey("LEFT_BACK_ARM")) {
-	int degree = root["LEFT_BACK_ARM"].as<int>();
-	moveJoint(LEFT_BACK_ARM, degree);
+	  int degree = root["LEFT_BACK_ARM"].as<int>();
+	  moveJoint(LEFT_BACK_ARM, degree);
   }
   
-  //send message
-  char JSONmessageBuffer[200];
-  root.printTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
+  //send feedback message
+  /*String output;
+  serializeJson(doc, output);
+  char output_char[output.length()];
+  output.toCharArray(output_char, output.length());
   Serial.println("Sending message to MQTT topic..");
-  Serial.println(JSONmessageBuffer);
-  if (client.publish(topic_output, JSONmessageBuffer) == true) {
+  Serial.println(output);
+  if (client.publish(topic_output, output_char) == true) {
     Serial.println("Success sending message");
   } else {
     Serial.println("Error sending message");
-  }
+  }*/
 }
-
-//reconnect to mqtt
-void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Create a random client ID
-    String clientId = "ESP8266Client-";
-    clientId += String(random(0xffff), HEX);
-    // Attempt to connect
-    if (client.connect(clientId.c_str())) {
-      Serial.println("connected");
-      // Once connected, publish an announcement...
-      client.publish(topic_input, "hello world");
-      // ... and resubscribe
-      client.subscribe(topic_input);
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
-}
-
 
 void setup() {
   pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
   Serial.begin(115200);
   setup_wifi();
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
+  setup_mqtt();
+  setup_adafruit_board();
 }
 
 void loop() {
-
+  //loop for mqtt
   if (!client.connected()) {
-    reconnect();
+    long now = millis();
+    if (now - lastReconnectAttempt > 5000) { // Try to reconnect.
+      lastReconnectAttempt = now;
+      if (reconnect()) { // Attempt to reconnect.
+        lastReconnectAttempt = 0;
+      }
+    }
+  } 
+  else { // Connected.
+    client.loop();
+    client.publish(topic_output, "Hello world!"); // Publish message.
+    delay(1000);
   }
-  client.loop();
 }
